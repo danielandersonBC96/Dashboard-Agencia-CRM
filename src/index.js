7187f06b740e6ebeacd5ead7b171f2213f99600e
danielandersonBC96 <danielandersonbrandaocampos@hotmail.com>
2023-08-08 16:05:01 -0400

    create config react-redux and Dark mode

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { configureStore } from '@reduxjs/toolkit';
+import globalReducer from 'States'
+import { Provider } from 'react-redux';
+
+const store = configureStore({
+  reducer:{
+    global: globalReducer,
+  },
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <Provider  store={store}>
+       <App/>
+     </Provider>
   </React.StrictMode>
 );
-
+git
 reportWebVitals();

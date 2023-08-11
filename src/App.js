63ad72d0db2a41e28edc82d9056df9d1ee03a7ae
danielandersonBC96 <danielandersonbrandaocampos@hotmail.com>
2023-08-10 23:36:06 -0400

    create routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { createTheme } from "@mui/material";
+import { createTheme } from '@mui/material/styles';
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { themeSettings } from "theme";
+import {  CssBaseline, ThemeProvider} from '@mui/icons-material';
+import {createTheme} from  '@mui/icons-material '; 
+import  { useMemo } from 'react';
+import { useSettings} from 'theme';
+import { BrowserRouter, Route } from "react-router-dom";
+
+
 
 function App() {
+  
+const mode = useSelector((state) => state.global.mode);
+const theme = useMemo(() => createTheme(themeSettings(mode), [mode]));
+
   return (
     <div className="App">
-     <h1>Hello World</h1>
+     <BrowserRouter>
+       <ThemeProvider theme={theme} >
+           <CssBaseline/>
+           <Routes >
+             <Route> </Route>
+
+           </Routes>
+       </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }

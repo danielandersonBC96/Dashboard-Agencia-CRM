f85b637c047e874eda8fd24c89f5cae6d0ea91ed
danielandersonBC96 <danielandersonbrandaocampos@hotmail.com>
2023-08-16 11:06:54 -0400

    create dark/light mode

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,71 @@
+
+import React , {useState}from 'react';
+import { 
+    LightOutlined, 
+    DarkModeOutlined,
+    Menu as MenuIcon ,
+    Search, 
+ArrowDropDownOutlined,
+SettingsOutlined,
+} from '@mui/icons-material';
+import FlexBetween from './Flexbetween';
+import { useDispatch } from 'react-redux';
+import { setMode } from 'States/States';
+import profileImage from 'assets/User-Icon.jpg';
+import {AppBar, IconButton, InputBase, Toolbar, useTheme} from '@mui/material';
+import { themeSettings } from 'theme';
+
+
+const Navbar = () => {
+
+    const dispatch = useDispatch()
+    const theme = useTheme();
+
+  return (
+    <AppBar
+      sx={{
+        position: "static",
+        background: "none",
+        boxShadow: "none",
+      }}
+     >
+         <Toolbar
+          sx={{
+            justifyContent: "space-between"
+
+
+          }}
+         >
+            <FlexBetween>
+                <IconButton  onClick={() => console.log('open/close_sidebar')} >   
+                  <MenuIcon/>
+                </IconButton>
+                <FlexBetween
+                  backgroundColor={ theme.palette.background.alt}
+                  borderRadius="9px"
+                  gap= "3rem"
+                  p="0.1rem 1.5rem"          
+                >
+                    <InputBase placeholder='Search'/>
+                    <Search/>
+
+                </FlexBetween>
+                <FlexBetween gap="1.5rem">
+                    <IconButton  onClick={() => dispatch(setMode())}>
+                      {theme.palette.mode === "dark" ? (
+                        <DarkModeOutlined sx={{ fontSize: "25px" }} />
+                            ) : (
+                         <LightOutlined sx={{ fontSize: "25px" }} />
+                           )}
+                    </IconButton>
+                    <IconButton>
+                        <SettingsOutlined sx={{fontSize:"25px"}}/>
+                    </IconButton>
+                </FlexBetween>
+            </FlexBetween>
+         </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Navbar;
--- src/Pages/Layout/index.jsx
+++ src/Pages/Layout/index.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { Box, useMediaQuery } from '@mui/material';
+import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import NavBar from 'Components/Navbar';
+import Navbar from 'Components/Navbar';
 
 const  Layout = () => {
   return (
-    <div><h1>  Admin Dashboard</h1></div>
+    <div>
+      <h1>  Admin Dashboard</h1>
+       <Box width= "100%"  height="100%">
+        <Box>
+          <Navbar/>
+          <Outlet/>
+        </Box>
+       </Box>
+    </div>
   )
 }
 
-export default  Layout
+export default  Layout; 
